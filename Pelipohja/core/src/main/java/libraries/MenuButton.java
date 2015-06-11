25f78bf19693bf0efd6422f1837bd32fb21ef0a9
Mikko Kumara <mikko.kumara@gmail.com>
2015-06-11 07:22:35 +0300

    assetteja ja keybindit menubuttoneihin

--- Pelipohja/core/src/main/java/libraries/MenuButton.java
+++ Pelipohja/core/src/main/java/libraries/MenuButton.java
@@ -1,5 +1,6 @@
 package libraries;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
@@ -10,6 +11,8 @@ public class MenuButton extends ClickAble {
     private final BitmapFont font = new BitmapFont();
     private Sprite defaultImage;
     private Sprite hoverImage;
+    private int key;
+    private boolean alreadyPressed;
 
     public MenuButton(float x, float y, Sprite defaultImage, Sprite hoverImage, String buttonText) {
         super(x, y, defaultImage);
@@ -35,6 +38,23 @@ public class MenuButton extends ClickAble {
     public String getButtonText(){
         return this.buttonText;
     }
+    
+    public void bindKey(int key) {
+        this.key = key;
+    }
+    
+    public boolean keyWasJustPressed() {
+        if (!Gdx.input.isKeyPressed(key)) {
+            alreadyPressed = false;
+            return false;
+        }
+        if (alreadyPressed) {
+            return false;
+        }
+        alreadyPressed = true;
+        return true;
+    }
 
     
 }
+
