85db98116225c34cdcd43b33b011bd90d30baa03
Virpi <virpi.sumu@gmail.com>
2016-06-13 16:18:11 +0300

    Lisätty mahdollisuus kuvan luomiseen String-osoitteen avulla

--- Pelipohja/core/src/main/java/libraries/Kuva.java
+++ Pelipohja/core/src/main/java/libraries/Kuva.java
@@ -8,5 +8,9 @@ public class Kuva extends PeliOtus{
     public Kuva(float x, float y, Sprite kuva) {
         super(x, y, kuva);
     }
+    
+    public Kuva(float x, float y, String kuvanOsoite) {
+        super(x, y, kuvanOsoite);
+    }
 
 }
