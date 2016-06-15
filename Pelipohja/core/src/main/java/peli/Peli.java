a04f6762bb693b29dbde8ac3c59be027bf1c88e7
Virpi <virpi.sumu@gmail.com>
2016-06-15 16:47:00 +0300

    pelin yhteinen public kuuntelija lisätty

--- Pelipohja/core/src/main/java/peli/Peli.java
+++ Pelipohja/core/src/main/java/peli/Peli.java
@@ -2,25 +2,37 @@
 package peli;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import libraries.Skenelista;
+import libraries.Kuuntelija;
 
 class Peli extends Game{
 
-    static final int SCREEN_WIDTH = 1200;    
-    static final int SCREEN_HEIGHT = 900;
+    public static final int SCREEN_WIDTH = 1200;    
+    public static final int SCREEN_HEIGHT = 900;
     
-    static Skenelista skenelista;
+    public static Skenelista skenelista;
+    public static Kuuntelija kuuntelija;
     
     
 
     @Override
-    public void create() {        
+    public void create() {  
+        // Tässä tehdään pelin rakenteen valmisteluja, joihin ei tarvitse koskea
         skenelista = new Skenelista();
+        kuuntelija = new Kuuntelija();        
+        Gdx.input.setInputProcessor(kuuntelija);
         
-        // Tässä luodaan pelissä käytetyt Skenet
+        // Tässä luodaan pelissä käytetyt Skenet.
         ValikkoSkene menu = new ValikkoSkene();
         menu.alusta();
         skenelista.lisaaSkene(menu);
+        
+        // Tähän alle voi lisätä pelin muut skenet, esim.
+        // PeliSkene peli = new PeliSkene();
+        // peli.alusta();
+        // skenelista.lisaaSkene(peli);
+        
     }
 
     @Override
